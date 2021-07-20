84a1e3891acd98106c2a51b9e925b579a395e65a
Quentinx6 <quentin.lecompte@3wa.io>
2021-07-20 12:12:09 +0200

    first commit

---
+++ 01_bases.js
@@ -0,0 +1,16 @@
+// https://developer.mozilla.org/fr/docs/Web/JavaScript
+//              Ressource
+/*  Je peux ecrire des commentaires ici
+
+
+
+
+
+*/
+console.log("Hello");
+console.warn("Attention");
+console.error("Ici il y a une erreur");
+
+var maVariable = "Je suis une variable d'ancienne génération et globale";
+let maLet = "21";
+const Const = "2000";
